7ae7bd96839fe8580b43c0375458ea12b1c6ccbb
Ignacio Ojea <iojea@dm.uba.ar>
2026-01-04 21:51:27 -0300

    adding polys

--- test/basis.jl
+++ test/basis.jl
@@ -1,22 +1,23 @@
 using ..TrihpFEM.Poly
 using Test
 using StaticArrays
+using Polynomials
 
 @test_throws ArgumentError("Degrees does not satisfy p conformity.") StandardBasis((1,2,4))
 
 B = StandardBasis((2,3,4))
-res0 =  [1.0   0.0  -0.5    0.0;
-         0.0   0.0  -0.0    0.0;
-        -0.5  -0.0   0.25  -0.0]
+p₀ = Polynomial((1.,))
+p₁ = Polynomial((0.,1.))
+p₂ = Polynomial((-1.,0.,3))/2
+p₃ = Polynomial((0.,-3,0,5))/2
+l₂ = [p₀,p₁,p₂]
+l₃ = [p₀,p₁,p₂,p₃]
 
-res023077 = [1.0       0.77     0.38935    -0.0136675;
-             0.23      0.1771   0.0895505  -0.00314352;
-            -0.42065  -0.3239  -0.16378     0.00574923]
 k = 0
 for b in B
     (i,j) = degs(b)
-    @test res0[i+1,j+1] ≈ b([0.,0.])
-    @test res023077[i+1,j+1] ≈ b(SVector(0.23,0.77))
+    @test l₂[i+1](0.)*l₃[j+1](0.) ≈ b([0.,0.])
+    @test l₂[i+1](0.23)*l₃[j+1](0.77) ≈ b(SVector(0.23,0.77))
     global k = k+1
 end
 
